2cf475425be5736b825f08563bf4016e5b8a1587
Alina Husarava <alinagusarik@gmail.com>
2022-06-02 18:36:35 +0300

    feat: task fs/read complete

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,3 +1,16 @@
-export const read = async () => {
-    // Write your code here 
+import * as fs from 'fs';
+import path from 'path';
+const __dirname = path.resolve( path.dirname( 'src/fs/files' ) );
+const errorMsg = new Error('FS operation failed');
+
+export const read = async () =>
+{
+const stream = fs.createReadStream(path.join(__dirname, 'files/fileToRead.txt'), 'utf-8');
+let data = '';
+stream.on('data', (chunk) => (data += chunk));
+stream.on('end', () => console.log(data));
+stream.on('error', (error) => console.error( errorMsg.message));
+
 };
+
+read()
