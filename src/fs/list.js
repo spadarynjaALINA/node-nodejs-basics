2662d03ee1e854298f778ee2aea988fa03c86149
Alina Husarava <alinagusarik@gmail.com>
2022-06-02 18:14:26 +0300

    feat: task fs/list complete

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,3 +1,18 @@
-export const list = async () => {
-    // Write your code here 
+import * as fs from 'fs';
+import path from 'path';
+const __dirname = path.resolve( path.dirname( 'src/fs/files' ) );
+const errorMsg = new Error('FS operation failed');
+const arrNames =[]
+export const list = async () =>
+{
+ fs.readdir(path.join(__dirname, 'files/'), { withFileTypes: true }, function (err, items) {
+  if (err) {console.error(errorMsg.message);}else {items.forEach(function (item) {
+    let name = path.parse(item.name).name;
+arrNames.push(name)
+
+  } );
+  console.log(arrNames)}
+});
 };
+
+list()
