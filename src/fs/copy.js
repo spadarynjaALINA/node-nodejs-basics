13997fe9b884c654671132671095da827e0ac0bb
Alina Husarava <alinagusarik@gmail.com>
2022-06-02 16:31:11 +0300

    feat:task fs/copy complete

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,3 +1,45 @@
-export const copy = async () => {
-    // Write your code here 
-};
+import * as fs from 'fs';
+import path from 'path';
+import { access, constants } from 'node:fs';
+const __dirname = path.resolve( path.dirname( 'src/fs/files' ) );
+const files = path.join(__dirname, 'files');
+const filesCopy = path.join(__dirname, 'filesCopy');
+const errorMsg = new Error('FS operation failed');
+
+export const copy = async () =>
+{
+  fs.access( files, ( err ) =>
+  {
+    if ( err )
+    {
+console.error( errorMsg.message)
+
+    } else
+    {
+       fs.access( filesCopy, ( err ) =>
+      {
+        if ( err )
+        {
+          fs.mkdir( filesCopy, { recursive: true }, ( err ) =>
+          {
+            if ( err ) throw err;
+          } );
+
+         fs.readdir(files, function (err, items) {
+          if (err) console.error(errorMsg.message)
+          items.forEach(function (item) {
+            fs.copyFile(path.join(__dirname, 'files', item), path.join(__dirname, 'filesCopy', item), (err) => {
+              if (err) throw err;
+            });
+          });
+        } );
+
+  }else
+        {
+          console.error( errorMsg.message)
+        }
+      } )
+    }
+  } )
+}
+copy()
