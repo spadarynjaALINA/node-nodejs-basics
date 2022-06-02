b9abea3abc2ffb9e4bd2e40acb2b78f8d7bc38e3
Alina Husarava <alinagusarik@gmail.com>
2022-06-02 20:23:29 +0300

    refactor:task fs/read change

--- src/fs/read.js
+++ src/fs/read.js
@@ -5,12 +5,11 @@ const errorMsg = new Error('FS operation failed');
 
 export const read = async () =>
 {
-const stream = fs.createReadStream(path.join(__dirname, 'files/fileToRead.txt'), 'utf-8');
-let data = '';
-stream.on('data', (chunk) => (data += chunk));
-stream.on('end', () => console.log(data));
-stream.on('error', (error) => console.error( errorMsg.message));
-
+    fs.readFile( path.join( __dirname, 'files/fileToRead.txt' ), 'utf-8', ( err , data) =>
+    {
+        if ( err ) { console.error( errorMsg.message ); }
+         process.stdout.write( data)
+});
 };
 
 read()
