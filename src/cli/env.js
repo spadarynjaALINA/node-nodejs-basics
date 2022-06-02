1d099a6d49c0b989b81685665df82891e7eed771
Alina Husarava <alinagusarik@gmail.com>
2022-06-02 20:18:15 +0300

    feat: task cli/env complete

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,22 @@
-export const parseEnv = () => {
-    // Write your code here 
+import process from 'process'
+function objectFilter(obj) {
+
+let filtered={}
+  for(let key in obj) {
+
+      if ( key.includes( 'RSS_' ) )
+      {
+          filtered[key] = obj[key];
+         process.stdout.write( `${key} = ${filtered[key]}; ` )
+      }
+  }
+}
+export const parseEnv = () =>
+{
+    const env = process.env
+
+ objectFilter(env)
+
 };
+
+parseEnv()
