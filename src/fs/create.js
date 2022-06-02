ab7447f9bea11ed0e311f09c3436352a35eeeff9
Alina Husarava <alinagusarik@gmail.com>
2022-06-02 15:14:12 +0300

    feat: task fs/create complete

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,15 @@
-export const create = async () => {
-    // Write your code here 
+import * as fs from 'fs';
+import path from 'path';
+import { access, constants } from 'node:fs';
+const __dirname = path.resolve( path.dirname( 'src/fs/files' ) );
+const errorMsg = new Error('FS operation failed');
+export const create = async () =>
+{
+   access(path.join(__dirname,'files', 'fresh.txt'), constants.F_OK, (err) => {
+    err? fs.writeFile(path.join(__dirname,'files', 'fresh.txt'), 'I am fresh and young', (error) => {
+    if (error) return console.error(errorMsg.message);
+  }): console.error(errorMsg.message)
+});
+
 };
+create()
