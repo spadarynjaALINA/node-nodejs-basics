491bd9a5f795d39b216a328110716fbe91647118
Alina Husarava <alinagusarik@gmail.com>
2022-06-03 15:22:25 +0300

    feat: task hash complete

--- src/cli/args.js
+++ src/cli/args.js
@@ -3,6 +3,7 @@ import process from 'process'
 
 export const parseArgs = () =>
 {
+
     const args = process.argv.slice( 2 )
    args.map( ( item, index ) =>
     {if((index % 2===0 || index ===0))process.stdout.write(index !== args
