d0bec33e02c83f19bed2bbf51f85ab0cb4b2571d
Alina Husarava <alinagusarik@gmail.com>
2022-06-05 19:54:12 +0300

    feat: task cp complete

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,3 +1,20 @@
-export const spawnChildProcess = async (args) => {
-    // Write your code here
+import {spawn} from'child_process';
+import path from 'path';
+const __dirname = path.resolve( path.dirname( 'src/cp/files' ) );
+const source = path.join(__dirname, 'files/script.js');
+const opts = {
+    stdio:['pipe', 'pipe', 'pipe','ipc']
 };
+
+export const spawnChildProcess = async ( ...args ) =>
+{
+
+  const node = spawn('node', [source, ...args],opts);
+
+process.stdin.pipe(node.stdin)
+node.stdout.pipe(process.stdout)
+node.on('close', (code) => {
+  console.log(`child process exited with code ${code}`);
+});
+};
+spawnChildProcess( 'args1', 'args2', 'args3' )
